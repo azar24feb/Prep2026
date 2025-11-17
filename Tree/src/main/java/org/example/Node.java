62b2dbfe565b99aa5e96dec1c3e8d4f90792a4b1
Azar <azar24feb@gmail.com>
2025-11-17 23:28:18 +0530

    yes

--- Tree/src/main/java/org/example/Node.java
+++ Tree/src/main/java/org/example/Node.java
@@ -6,7 +6,33 @@ public class Node {
     public Node left;
     public Node right;
 
-    public Node (int key) {
+    public Node(int key) {
         this.key = key;
+    }
+
+    public static void inOrder(Node root) {
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.println(root.key);
+        inOrder(root.right);
+    }
+
+    public static void preOrder(Node root) {
+        if (root == null) return;
+        System.out.println(root.key);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.key);
+    }
+
+    public static int height(Node root) {
+        if (root == null) return 0;
+        return Integer.max(height(root.left), height(root.right)) + 1;
     }
 }
--- Tree/src/main/java/org/example/TreeTester.java
+++ Tree/src/main/java/org/example/TreeTester.java
@@ -1,9 +1,22 @@
-//package org.example;
+package org.example;
 
 import org.example.Node;
 
-void main() {
-//    Node root = new Node(10);
-//    root.left = new Node(20);
-    System.out.println("hello");
+public class TreeTester{
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(7);
+        root.left.right.right = new Node(8);
+        root.right = new Node(3);
+        root.right.right = new Node(6);
+
+//        Node.inOrder(root);
+        Node.preOrder(root);
+//         Node.postOrder(root);
+        int h = Node.height(root);
+        System.out.printf("Height %s", h);
+    }
 }
